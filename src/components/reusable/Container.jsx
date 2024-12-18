a09f10f71d2992e968b69e0396918ee92311f42e
Abhijeet Kumar <abhijeetthakur7080@gmail.com>
2024-12-18 19:15:35 +0530

    Update

--- src/components/reusable/Container.jsx
+++ src/components/reusable/Container.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Container = ({ children }) => {
-  return <div className="w-full h-[calc(100vh-120px)] sm:h-[calc(100vh-100px)] bg-gray-900 overflow-y-auto">{children}</div>;
+  return <div className="w-full h-[calc(100vh-120px)] mb-8 sm:mb-0 sm:h-[calc(100vh-100px)] bg-gray-900 overflow-y-auto">{children}</div>;
 };
 
 export default Container;
